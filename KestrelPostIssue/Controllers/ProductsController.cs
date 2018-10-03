d034c1bbea55897fc0a14b46a09c4fb1e78657f3
Cibin <cibin_jose@infosys.com>
2018-10-03 14:38:20 +0530

    Initial commit

---
+++ KestrelPostIssue/Controllers/ProductsController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace KestrelPostIssue.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+       
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody] Product value)
+        {
+            Console.Write($"ADD {value?.Name}");
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] Product value)
+        {
+            Console.Write($"UPDATE {value?.Name}");
+        }        
+    }
+}
